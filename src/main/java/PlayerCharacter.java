97f99c8304fa0583fc3f87d677d49f8d87e875b8
daniel-pohl <danielpohl@mail.de>
2024-04-18 11:21:16 +0200

    second commit

---
+++ src/main/java/PlayerCharacter.java
@@ -0,0 +1,13 @@
+public class PlayerCharacter {
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+    public static int getX() {
+        return 0;
+    }
+
+}
---
+++ src/test/java/PlayerCharacterTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PlayerCharacterTest {
+
+    @Test
+    public void getXReturn0(){
+        //GIVEN
+
+        //WHEN
+        int actual = PlayerCharacter.getX();
+        //THEN
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+
+}
